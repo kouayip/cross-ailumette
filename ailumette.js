9bca0af4401144a441e27574499ae6a4d70b26ff
Yves Kouayip (L'amchard) <kamwayves37@gmail.com>
2021-02-21 23:36:39 +0100

    Added IA and logic game

--- ailumette.js
+++ ailumette.js
@@ -3,6 +3,7 @@ const prompt = require("prompt-sync")();
 
 const TYPE_COMPONENT = {
   UNKNOWN: "Unknown",
+  MAP: "Map",
   DRAWABLE: "Drawable",
   IA: "Ia",
   INPUT: "Input",
@@ -14,6 +15,61 @@ const ENTITY_TYPE = {
   COMPUTER: "Computer",
   GRAPHICS: "Graphics",
 };
+
+// Matrix is class use to create a location for ailumette
+class Matrix {
+  constructor(rows, cols) {
+    this._rows = rows;
+    this._cols = cols;
+    this._values = [];
+  }
+  init() {
+    // for (let i = 0; i < this._rows; i++) {
+    //   this._values.push([]);
+    //   for (let j = 0; j < this._cols; j++) {
+    //     this._values[i].push(0);
+    //   }
+    // }
+    // this._values.for;
+    this._values = [
+      [0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 1, 1, 1, 0, 0],
+      [0, 1, 1, 1, 1, 1, 0],
+      [1, 1, 1, 1, 1, 1, 1],
+    ];
+  }
+
+  getRows() {
+    return this._rows;
+  }
+
+  getCols() {
+    return this._cols;
+  }
+
+  getValue(row, col) {
+    return this._values[row][col];
+  }
+
+  setValue(row, col, value) {
+    return (this._values[row][col] = value);
+  }
+
+  checkRowIsEmpty(row) {
+    for (let j = 0; j < this._cols; j++) {
+      if (this.getValue(row, j) === 1) return false;
+    }
+    return true;
+  }
+
+  countActiveCol(row) {
+    let cpt = 0;
+    for (let j = 0; j < this._cols; j++) {
+      if (this.getValue(row, j) === 1) cpt++;
+    }
+    return cpt;
+  }
+}
 
 /*** START COMPONENT ****/
 class Component {
@@ -37,6 +93,28 @@ class Component {
   }
 }
 
+class MapComponent extends Component {
+  constructor() {
+    super();
+    this._map = new Matrix(4, 7);
+  }
+
+  getMap() {
+    return this._map;
+  }
+
+  type() {
+    return TYPE_COMPONENT.MAP;
+  }
+  init() {
+    console.log("=> Init MAP Component");
+    this._map.init();
+  }
+  update() {
+    console.log("=> update map Component");
+  }
+}
+
 class DrawableComponent extends Component {
   type() {
     return TYPE_COMPONENT.DRAWABLE;
@@ -46,15 +124,22 @@ class DrawableComponent extends Component {
   }
   update() {}
   draw() {
-    console.log(
-      chalk.gray(`*********
-*   |   *
-*  |||  *
-* ||||| *
-*|||||||*
-*********
-`)
-    );
+    console.log("*********");
+
+    const map = this._entity.getComponent(TYPE_COMPONENT.MAP).getMap();
+
+    let view = "";
+    for (let i = 0; i < map.getRows(); i++) {
+      view = "*";
+      for (let j = 0; j < map.getCols(); j++) {
+        if (map.getValue(i, j) === 1) view += "|";
+        else view += " ";
+      }
+      view += "*";
+      console.log(view);
+    }
+
+    console.log("*********\n");
   }
 }
 
@@ -91,6 +176,20 @@ class VisitorComponent extends Component {
   update() {
     console.log("Update Visitor");
     console.log(`line: ${this._locate.line} matches: ${this._locate.matches}`);
+    const map = this._entity
+      .getWord()
+      .getEntity(ENTITY_TYPE.GRAPHICS)
+      .getComponent(TYPE_COMPONENT.MAP)
+      .getMap();
+
+    let cpt = 0;
+    for (let j = map.getCols() - 1; j >= 0; j--) {
+      if (map.getValue(this._locate.line - 1, j) !== 0) {
+        map.setValue(this._locate.line - 1, j, 0);
+        cpt++;
+      }
+      if (cpt === this._locate.matches) break;
+    }
   }
 }
 
@@ -106,14 +205,44 @@ class InputComponent extends Component {
   }
   update() {
     console.log("Your turn:");
-    const line = Number(prompt("Line: "));
-    const matches = Number(prompt("Matches: "));
+    /*** CHECK LINE ***/
+    let line;
+    do {
+      line = Number(prompt("Line: "));
+
+      if (line <= 0 || Number.isNaN(line)) {
+        console.log("Error: invalid input (positive number expected)");
+      }
+
+      if (line > 4) {
+        console.log("Error: this line is out of range");
+      }
+    } while (line > 4 || line <= 0 || Number.isNaN(line));
+
+    /*** CHECK MATCHES ***/
+    let matches;
+    do {
+      matches = Number(prompt("Matches: "));
+
+      if (matches <= 0 || Number.isNaN(matches)) {
+        console.log("Error: invalid input (positive number expected)");
+      }
+
+      if (matches > 7) {
+        console.log("Error: not enough matches on this line");
+      }
+    } while (matches > 7 || matches <= 0 || Number.isNaN(matches));
+
+    console.log(`Player removed ${matches} match(es) from line ${line}`);
     const visitor = this._entity.getComponent(TYPE_COMPONENT.VISITOR);
     visitor.setLocate(line, matches);
   }
 }
 
 class IAComponent extends Component {
+  getRandom(min, max) {
+    return Math.floor(Math.random() * max) + min;
+  }
   type() {
     return TYPE_COMPONENT.IA;
   }
@@ -122,13 +251,26 @@ class IAComponent extends Component {
   }
   update() {
     console.log("AI’s turn...");
+    const map = this._entity
+      .getWord()
+      .getEntity(ENTITY_TYPE.GRAPHICS)
+      .getComponent(TYPE_COMPONENT.MAP)
+      .getMap();
+
+    let line;
+    do {
+      line = this.getRandom(1, 4);
+    } while (map.checkRowIsEmpty(line - 1));
+
+    const matches = this.getRandom(1, map.countActiveCol(line - 1));
+
+    console.log(`AI  removed ${matches} match(es) from line ${line}`);
+    const visitor = this._entity.getComponent(TYPE_COMPONENT.VISITOR);
+    visitor.setLocate(line, matches);
   }
 }
 
 /*** END COMPONENT ****/
-
-// CLI console use to print output message
-class CLI {}
 
 class Entity {
   /**
@@ -149,6 +291,10 @@ class Entity {
    */
   setWord(word) {
     this._word = word;
+  }
+
+  getWord() {
+    return this._word;
   }
 
   /**
@@ -202,18 +348,18 @@ class Entity {
   }
 }
 
-// Matrix is class use to create a location for ailumette
-class Matrix {
-  constructor(rows, cols) {
-    this._rows = rows;
-    this._cols = cols;
-  }
-}
-
 class World {
   constructor() {
     this._entities = {};
     this._graphicsSystem = {};
+  }
+
+  isFinish() {
+    const map = this._graphicsSystem.getComponent(TYPE_COMPONENT.MAP).getMap();
+    for (let i = 0; i < map.getRows(); i++) {
+      if (!map.checkRowIsEmpty(i)) return false;
+    }
+    return true;
   }
 
   /**
@@ -223,8 +369,9 @@ class World {
   registerEntity(entity) {
     entity.setWord(this);
     this._entities[entity.type()] = entity;
-    if (entity.type() === ENTITY_TYPE.GRAPHICS)
-      this._graphicsSystem[entity.type()] = entity;
+    if (entity.type() === ENTITY_TYPE.GRAPHICS) {
+      this._graphicsSystem = entity;
+    }
   }
 
   /**
@@ -239,25 +386,27 @@ class World {
   }
 
   update() {
-    Object.values(this._entities).forEach((entity) => {
-      switch (entity.type()) {
-        case ENTITY_TYPE.PLAYER:
-          entity.update();
-          this.draw();
-          break;
-        case ENTITY_TYPE.COMPUTER:
-          entity.update();
-          this.draw();
-          break;
-        default:
-          entity.update();
-          break;
-      }
-    });
+    do {
+      Object.values(this._entities).forEach((entity) => {
+        switch (entity.type()) {
+          case ENTITY_TYPE.PLAYER:
+            entity.update();
+            this.draw();
+            break;
+          case ENTITY_TYPE.COMPUTER:
+            entity.update();
+            this.draw();
+            break;
+          default:
+            entity.update();
+            break;
+        }
+      });
+    } while (!this.isFinish());
   }
 
   draw() {
-    Object.values(this._graphicsSystem).forEach((entity) => entity.draw());
+    this._graphicsSystem.draw();
   }
 }
 
@@ -271,9 +420,9 @@ class GameEngine {
   }
 }
 
-const game = new GameEngine();
+// const game = new GameEngine();
 
-game.start();
+// game.start();
 
 const word = new World();
 
@@ -289,6 +438,7 @@ computer.attach(new VisitorComponent());
 
 /** GRAPHICS */
 const graphics = new Entity(ENTITY_TYPE.GRAPHICS);
+graphics.attach(new MapComponent());
 graphics.attach(new DrawableComponent());
 
 word.registerEntity(player);
@@ -298,6 +448,4 @@ word.registerEntity(graphics);
 //? Test
 word.init();
 word.draw();
-for (let index = 0; index < 2; index++) {
-  word.update();
-}
+word.update();
